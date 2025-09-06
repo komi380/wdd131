f2379e8a2d5ed440edbdf5e29255dc502031777f
yourusername <boomp380@gmail.com>
2025-09-06 10:40:04 +0000

    Initial commit

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Display current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Display last modified date
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
